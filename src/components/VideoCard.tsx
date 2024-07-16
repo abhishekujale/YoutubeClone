f7a75fc6110c9f9ba43833ca000e2b39b0d0ab2c
abhishekujale <abhishekujale.kitcse18@gmail.com>
2024-07-16 18:26:58 +0530

    Almost Done

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,30 +1,31 @@
+import { useRouter } from "next/router"
+import type { Video } from "../videos"
+import { Line } from "./Line"
 
-const VideoCard = (props:any) => {
-  return (
-    <div className=" mt-5 ml-5 ">
-          <img  className="rounded-3xl" src={props.image} alt="" />
-          <div className="grid grid-cols-12">
-              <div className="col-span-1">
-          <img className="rounded-full w-30 h-30 mt-3 " src={props.thumb}   alt="" />
-              </div>
-              <div className="col-span-11 mt-5 ml-5">
-                  
-                  <div >
-                 {props.title}
-                  </div>
-
-                  <div className="col-span-11  text-gray-500 text-base  ">
-                  {props.author}
-                  </div>
-
-                   <div className="col-span-11  text-gray-500 text-base">
-                   {props.views} | {props.timestamp}
-                   </div>
-              </div>
-              
-          </div>
+export const VideoCard = ({video}: {video: Video}) => {
+    const router = useRouter();
+    return <div className="cursor-pointer" onClick={() => {
+        router.push("/video/1");
+    }}>
+        <div className="rounded-xl overflow-hidden">
+            <div>
+                <img src={video.thumbnail} />
+                <Line progress={10} />
+            </div>
+        </div>
+        <div className={"text-white-800 text-2xl font-medium"}>
+            {video.title}
+        </div>
+        <div className={"text-gray-400 text-xl font-normal		"}>
+            {video.description}
+        </div>
+        <div className="flex">
+            <div className={"text-gray-400 text-xl font-normal	pr-2	"}>
+                {video.viewCount} 
+            </div>
+            <div className={"text-gray-400 text-xl font-normal		"}>
+            • {video.timestamp}
+            </div>
+        </div>
     </div>
-  )
 }
-
-export default VideoCard
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,21 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-// import VideoCard from "../components/VideoCard"
-import VideoCard from "@/components/VideoCard"
-const inter = Inter({ subsets: ["latin"] });
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { VideoCard } from '@/components/VideoCard'
+import { VIDEOS } from '@/videos'
+import { VideoGrid } from '@/components/VideoGrid'
+import { Appbar } from '@/components/Appbar'
+import { LeftBar } from "../components/Leftbar"
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className="bg-grey-500">
-      <VideoCard
-        title={"Tailwind CSS is a utility-first CSS framework for rapidly building modern websites without ever leaving your HTML."}
-        views={100000}
-        author={"Abhishek Ujale"}
-        timestamp={"37 days ago"}
-        image={"/photo.png"}
-        thumb={"/thumb.jpg"} />
-    </div>
-  );
+    <main>
+      <Appbar/>
+      <div className="flex">
+        <LeftBar />
+        <VideoGrid />
+      </div>
+    </main>
+  )
 }
